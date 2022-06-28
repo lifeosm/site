4d620e2183bc9fe679c8242d4cfcd215e1cda585
Kamil Samigullin <kamil@samigullin.info>
2022-06-28 10:27:20 +0300

    feat(deps): add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,10 @@
-import {defineConfig} from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
