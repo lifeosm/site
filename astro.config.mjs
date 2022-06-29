5e982d9d1139f01080dae7b9960ab41add13ab59
Kamil Samigullin <kamil@samigullin.info>
2022-06-29 08:52:33 +0300

    dev: refactor Hero component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  })]
 });
